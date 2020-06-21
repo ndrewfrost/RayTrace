926dd8f64910bea8d749d3f210dfb96d0407a741
Andrew <21962412+ndrewfrost@users.noreply.github.com>
2020-06-21 18:51:56 +0100

    sphere record material when hit

--- geometry/sphere.cpp
+++ geometry/sphere.cpp
@@ -7,45 +7,49 @@
 
 #include "sphere.h"
 
-///////////////////////////////////////////////////////////////////////////
-// Sphere                                                                //
-///////////////////////////////////////////////////////////////////////////
+ ///////////////////////////////////////////////////////////////////////////
+ // Sphere                                                                //
+ ///////////////////////////////////////////////////////////////////////////
 
-//-------------------------------------------------------------------------
-// Given a ray, returns true if ray intersects sphere
-// else returns false
-//
-bool Sphere::intersect(const Ray & ray, float tMin, float tMax, HitRecord& record)
-{    
-    glm::vec3 oc = ray.origin() - m_center;    
-    float a      = glm::dot(ray.direction(), ray.direction());
-    float half_b = /* (2 */  glm::dot(oc, ray.direction());
-    float c      = glm::dot(oc, oc) - m_radius * m_radius;
+ //-------------------------------------------------------------------------
+ // Given a ray, returns true if ray intersects sphere
+ // else returns false
+ //
+bool Sphere::intersect(const Ray& ray, float tMin, float tMax, HitRecord& record)
+{
+    glm::vec3 oc = ray.origin() - m_center;
+    float a = glm::dot(ray.direction(), ray.direction());
+    float halfB = /* (2 */  glm::dot(oc, ray.direction());
+    float c = glm::dot(oc, oc) - sqr(m_radius);
 
-    float discriminant = half_b * half_b - a * c;
+    float discriminant = sqr(halfB) - a * c;
     float root = std::sqrtf(discriminant);
 
     // Hit
-    if (discriminant > 0) {        
-        float t = (-half_b - root) / a;
-        if ( t < tMax && t > tMin) {
-            record.t = t;
-            record.point = ray.at(record.t);
-            glm::vec3 outNormal = (record.point - m_center) / m_radius;
-            record.setFaceNormal(ray, outNormal);
-            return true;
-        }
-        t = (half_b + root) / a;
+    if (discriminant > 0) {
+
+        float t = (-halfB - root) / a;
         if (t < tMax && t > tMin) {
             record.t = t;
             record.point = ray.at(record.t);
             glm::vec3 outNormal = (record.point - m_center) / m_radius;
             record.setFaceNormal(ray, outNormal);
+            record.material = m_material;
+            return true;
+        }
+
+        t = (-halfB + root) / a;
+        if (t < tMax && t > tMin) {
+            record.t = t;
+            record.point = ray.at(record.t);
+            glm::vec3 outNormal = (record.point - m_center) / m_radius;
+            record.setFaceNormal(ray, outNormal);
+            record.material = m_material;
             return true;
         }
     }
 
     // Miss
     return false;
-    
+
 }
